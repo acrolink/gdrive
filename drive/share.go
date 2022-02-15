1bdd3ea17dfd1c156f5355e0ab2e61e25dcc28ef
acrolink <contact@acro.link>
2022-02-15 10:58:45 +0200

    Update share.go

--- drive/share.go
+++ drive/share.go
@@ -26,15 +26,15 @@ func (self *Drive) Share(args ShareArgs) error {
 		Domain:             args.Domain,
 	}
 	
-   call := self.service.Permissions.Create(args.FileId, permission)                                                                                 
+    call := self.service.Permissions.Create(args.FileId, permission)                                                                                 
     if permission.Role == "owner" {                                                                                                                  
         call.TransferOwnership(true)                                                                                                                 
-    }
-	
-	_, err := self.service.Permissions.Create(args.FileId, permission).Do()
-	if err != nil {
-		return fmt.Errorf("Failed to share file: %s", err)
-	}
+    }                                                                                                                                                
+                                                                                                                                                     
+    _, err := call.Do()                                                                                                                              
+    if err != nil {                                                                                                                                  
+        return fmt.Errorf("Failed to share file: %s", err)                                                                                           
+    } 
 
 	fmt.Fprintf(args.Out, "Granted %s permission to %s\n", args.Role, args.Type)
 	return nil
